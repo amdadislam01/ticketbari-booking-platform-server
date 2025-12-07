e842a511d059e80df3a35dbf99ca9eb1370e0e88
amdadisalm01 <amdadislam733@gmail.com>
2025-12-07 12:09:57 +0600

    ticket delete api complate

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -80,7 +80,13 @@ async function run() {
         .toArray();
       res.send(result);
     });
-    
+    // Ticket delete api
+    app.delete("/added-ticket/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await ticketCollection.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
