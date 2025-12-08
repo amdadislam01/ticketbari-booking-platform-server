ec1323705247737dc32d5357a0e7bb196e0a44a4
amdadisalm01 <amdadislam733@gmail.com>
2025-12-08 17:36:46 +0600

    Booking Ticekt added api complate

--- index.js
+++ index.js
@@ -25,49 +25,73 @@ async function run() {
     const db = client.db("ticketBari");
     const usersCollection = db.collection("users");
     const ticketCollection = db.collection("added-ticket");
+    const bookTicketCollection = db.collection("booking");
 
-    // Users Related API
+    // Create User
     app.post("/users", async (req, res) => {
       const user = req.body;
       user.role = "user";
       user.createAt = new Date();
-
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
 
-    // Users get
-    // app.get("/users", async (req, res) => {
-    //   const result = await usersCollection.findOne();
-    //   res.send(result);
-    // });
-
-    // Users Role
+    // Get User Role
     app.get("/users/:email/role", async (req, res) => {
       const email = req.params.email;
-      const query = { email };
-      const user = await usersCollection.findOne(query);
+      const user = await usersCollection.findOne({ email });
       res.send({ role: user?.role || "user" });
     });
 
+    // Get Single User
     app.get("/users/:email", async (req, res) => {
       const email = req.params.email;
-      const query = { email };
-      const user = await usersCollection.findOne(query);
+      const user = await usersCollection.findOne({ email });
       res.send(user);
     });
+    // Get All Users
+    app.get("/users", async (req, res) => {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // Users Update API
+    app.patch("/users/:id/role", async (req, res) => {
+      const id = req.params.id;
+      const { role } = req.body;
+      const query = { _id: new ObjectId(id) };
 
-    // Ticket Create API
+      await usersCollection.updateOne(query, { $set: { role } });
+
+      if (role === "fraud") {
+        await ticketCollection.updateMany(
+          { vendorId: id },
+          { $set: { status: "hidden" } }
+        );
+      }
+
+      res.send({ message: "Role updated & fraud process completed!" });
+    });
+
+    /// Create Ticket
     app.post("/added-ticket", async (req, res) => {
       const ticket = req.body;
-      const price = ticket.price;
-      const quantity = ticket.quantity;
-      // ticket Create Time
+      const user = await usersCollection.findOne({ email: ticket.vendorEmail });
+
+      if (user?.role === "fraud") {
+        return res.status(403).send({
+          success: false,
+          message: "You are marked as FRAUD. You cannot add new tickets.",
+        });
+      }
+
+      ticket.vendorId = user._id.toString();
       ticket.createAt = new Date();
       ticket.date = new Date(ticket.date);
       ticket.status = "pending";
-      ticket.price = Number(price);
-      ticket.quantity = Number(quantity);
+      ticket.price = Number(ticket.price);
+      ticket.quantity = Number(ticket.quantity);
+
       const result = await ticketCollection.insertOne(ticket);
       res.send(result);
     });
@@ -75,53 +99,133 @@ async function run() {
     // Get Tickets by Vendor
     app.get("/added-ticket", async (req, res) => {
       const email = req.query.email;
+
+      const user = await usersCollection.findOne({ email });
+      if (user?.role === "fraud") {
+        return res.send([]);
+      }
       const result = await ticketCollection
         .find({ vendorEmail: email })
         .toArray();
       res.send(result);
     });
-    // Ticket delete api
-    app.delete("/added-ticket/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await ticketCollection.deleteOne(query);
+
+    // Get Pending Tickets
+    app.get("/added-ticket/pending", async (req, res) => {
+      const result = await ticketCollection
+        .find()
+        .sort({ createAt: -1 })
+        .toArray();
       res.send(result);
     });
-    // Ticket Update Api
-    app.patch("/added-ticket/:id", async (req, res) => {
+    // Get Approved Tickets
+    app.get("/added-ticket/approved", async (req, res) => {
+      const result = await ticketCollection
+        .find({ status: "approved" })
+        .sort({ createAt: -1 })
+        .toArray();
+      res.send(result);
+    });
+    // Details Get API
+    app.get("/added-ticket/approved/:id", async (req, res) => {
       const id = req.params.id;
-      const updates = req.body;
-      if (updates.date) updates.date = new Date(updates.date);
+      const query = { _id: new ObjectId(id) };
+      const result = await ticketCollection.findOne(query);
+      res.send(result);
+    });
+    // Update Ticket Status
+    app.patch("/added-ticket/status/:id", async (req, res) => {
+      const id = req.params.id;
+      const { status } = req.body;
 
-      try {
-        const filter = { _id: new ObjectId(id) };
-        const updateDoc = { $set: updates };
+      const result = await ticketCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { status } }
+      );
 
-        const result = await ticketCollection.updateOne(filter, updateDoc);
-
-        if (result.matchedCount === 0) {
-          return res.status(404).send({ message: "Ticket not found" });
-        }
-
-        res.send({ message: "Ticket updated successfully", result });
-      } catch (error) {
-        res.status(500).send({ message: "Error updating ticket", error });
-      }
+      res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // Update Ticket
+    app.patch("/added-ticket/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const updates = req.body;
+
+      if (updates.date) updates.date = new Date(updates.date);
+
+      const result = await ticketCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updates }
+      );
+
+      res.send(result);
+    });
+
+    // Delete Ticket
+    app.delete("/added-ticket/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await ticketCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+    // Booking Ticket
+    app.post("/booking", async (req, res) => {
+      const { ticketId, quantity, ...rest } = req.body;
+
+      const ticket = await ticketCollection.findOne({
+        _id: new ObjectId(ticketId),
+      });
+      if (!ticket) {
+        return res
+          .status(404)
+          .send({ success: false, message: "Ticket not found" });
+      }
+
+      if (ticket.quantity < quantity) {
+        return res
+          .status(400)
+          .send({ success: false, message: "Not enough tickets available" });
+      }
+
+      //  ticket quantity
+      await ticketCollection.updateOne(
+        { _id: new ObjectId(ticketId) },
+        { $inc: { quantity: -quantity } }
+      );
+      let result;
+      const existBookingTicket = await bookTicketCollection.findOne({
+        ticketId,
+      });
+
+      if (existBookingTicket) {
+        result = await bookTicketCollection.updateOne(
+          { _id: existBookingTicket._id },
+          { $inc: { quantity: quantity } }
+        );
+      } else {
+        result = await bookTicketCollection.insertOne({
+          ticketId,
+          quantity,
+          ...rest,
+        });
+      }
+
+      res.send({ success: true, result });
+    });
+
+    console.log("Connected to MongoDB");
   } finally {
   }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Ticketbari-booking-platform is running !");
+  res.send("Ticketbari-booking-platform is running!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
